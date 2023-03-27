7ffb459b93b0bd3818d7448bc5c293a02db24e98
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-03-27 19:54:11 +0200

    Added FetchErrorContext and useFetchErrorContext hook.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import {Header} from './layouts/Header/Header';
 import {Main} from './layouts/Main/Main';
+import {FetchErrorProvider} from './contexts/FetchErrorContext';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <FetchErrorProvider>
+        <Header />
+        <Main />
+      </FetchErrorProvider>
     </div>
   );
 }
