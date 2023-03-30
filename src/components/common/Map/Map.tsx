e7be5995f25eca1bbf12cb112a527a5172425bcf
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-03-30 12:50:56 +0200

    Leaflet integrated, added Map component.

---
+++ src/components/common/Map/Map.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+import './Map.css';
+import {MapContainer, TileLayer} from 'react-leaflet';
+
+export const Map = () => {
+  const defaultCenter = {
+    lat: 0,
+    lng: 0,
+  };
+  const defaultZoom = 1;
+
+  return (
+    <div className="map">
+      <MapContainer center={defaultCenter} zoom={defaultZoom}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+};
