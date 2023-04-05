1d8ef1dea51156deaafddc6b654bd402fe0b95f1
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-04-05 16:23:18 +0200

    Added GameContent component.

---
+++ src/components/GameContent/GameContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './GameContent.css';
+import {Round} from '../Round/Round';
+
+export const GameContent = () => {
+  return (
+    <div className="game-content">
+      <Round />
+    </div>
+  );
+};
