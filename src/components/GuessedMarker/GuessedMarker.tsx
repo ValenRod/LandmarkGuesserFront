6e8192e6fd47b510b267bd6cc440f107d6eae441
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-04-06 21:47:20 +0200

    Icons added to GuessedMarker and markers in MarkerContainer.

--- src/components/GuessedMarker/GuessedMarker.tsx
+++ src/components/GuessedMarker/GuessedMarker.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {useGameContext} from '../../hooks/useGameContext';
 import {Marker, useMapEvents} from 'react-leaflet';
+import {useMarkerIcon} from '../../hooks/useMarkerIcon';
+import guessedMarkerIconUrl from '../../icons/avatar-svgrepo-com.svg';
 
 export const GuessedMarker = () => {
   const {guessedMarkerPosition, setGuessedMarkerPosition} = useGameContext();
+  const guessedMarkerIcon = useMarkerIcon({iconUrl: guessedMarkerIconUrl});
 
   useMapEvents({
     click(e) {
@@ -12,6 +15,9 @@ export const GuessedMarker = () => {
   });
 
   return guessedMarkerPosition ? (
-    <Marker position={guessedMarkerPosition} />
+    <Marker
+      position={guessedMarkerPosition}
+      icon={guessedMarkerIcon.markerIcon}
+    />
   ) : null;
 };
--- src/components/MarkersContainer/MarkersContainer.tsx
+++ src/components/MarkersContainer/MarkersContainer.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Marker, Polyline} from 'react-leaflet';
 import {Coordinates} from 'types';
+import {useMarkerIcon} from '../../hooks/useMarkerIcon';
+import landmarkMarkerIconUrl from '../../icons/flag-svgrepo-com.svg';
+import playerMarkerIconUrl from '../../icons/avatar-svgrepo-com.svg';
 
 export interface MarkersDataObj {
   landmarkMarker: Coordinates;
@@ -12,17 +15,26 @@ interface Props {
 }
 
 export const MarkersContainer = (props: Props) => {
+  const landmarkMarkerIcon = useMarkerIcon({iconUrl: landmarkMarkerIconUrl});
+  const playerMarkerIcon = useMarkerIcon({iconUrl: playerMarkerIconUrl});
   const {markers} = props;
+
   return (
     <div className="markers-container">
-      <Marker position={markers.landmarkMarker} />
+      <Marker
+        position={markers.landmarkMarker}
+        icon={landmarkMarkerIcon.markerIcon}
+      />
       <Polyline
         positions={[markers.landmarkMarker, markers.playerMarker]}
         dashArray={[1, 6]}
         weight={3}
         color={'black'}
       />
-      <Marker position={markers.playerMarker} />
+      <Marker
+        position={markers.playerMarker}
+        icon={playerMarkerIcon.markerIcon}
+      />
     </div>
   );
 };
