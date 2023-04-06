98c03c00750b48e9cdb9a9ea1b946099a98fe26e
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-04-06 21:50:51 +0200

    GameContext implemented in GameContent.

--- src/components/GameContent/GameContent.tsx
+++ src/components/GameContent/GameContent.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './GameContent.css';
 import {Round} from '../Round/Round';
+import {useGameContext} from '../../hooks/useGameContext';
+import {Summary} from '../Summary/Summary';
 
 export const GameContent = () => {
+  const {gameParams} = useGameContext();
+  const {rounds} = gameParams;
+
   return (
     <div className="game-content">
-      <Round />
+      {!rounds[rounds.length - 1].points ? <Round /> : <Summary />}
     </div>
   );
 };
