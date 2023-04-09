fcca6f32945d85ef4e76231d9fc195c0d40a8198
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-04-09 20:37:41 +0200

    Map component reworked

--- src/components/common/Map/Map.tsx
+++ src/components/common/Map/Map.tsx
@@ -3,38 +3,43 @@ import 'leaflet/dist/leaflet.css';
 import './Map.css';
 import {MapContainer, TileLayer} from 'react-leaflet';
 import {GuessedMarker} from '../../GuessedMarker/GuessedMarker';
-import {
-  MarkersContainer,
-  MarkersDataObj,
-} from '../../MarkersContainer/MarkersContainer';
+import {MarkersContainer} from '../../MarkersContainer/MarkersContainer';
+import {useGameContext} from '../../../hooks/useGameContext';
+import {useMarkersData} from '../../../hooks/useMarkersData';
+import {useMapZoomAndCenter} from '../../../hooks/useMapZoomAndCenter';
+import {useMapReload} from '../../../hooks/useMapReload';
 
-interface Props {
-  markersData?: MarkersDataObj[] | null;
-}
-
-export const Map = (props: Props) => {
-  const {markersData} = props;
-  const defaultZoom = 1;
-  const defaultCenter = {
-    lat: 0,
-    lng: 0,
-  };
+export const Map = () => {
+  const {loadMap, setReloadMap} = useMapReload();
+  const {gameParams, showGameSummary} = useGameContext();
+  const {rounds} = gameParams;
+  const {landmark, distance} = rounds[rounds.length - 1];
+  const {markersData} = useMarkersData({rounds, showGameSummary});
+  const {mapCenter, mapZoomLevel} = useMapZoomAndCenter({
+    lat: landmark.lat,
+    lng: landmark.lng,
+    distance,
+    showGameSummary,
+    setReloadMap,
+  });
 
   return (
     <div className="map">
-      <MapContainer center={defaultCenter} zoom={defaultZoom}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {!markersData ? (
-          <GuessedMarker />
-        ) : (
-          markersData.map((markers, index) => (
-            <MarkersContainer markers={markers} key={index} />
-          ))
-        )}
-      </MapContainer>
+      {loadMap ? (
+        <MapContainer center={mapCenter} zoom={mapZoomLevel}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {!markersData?.length ? (
+            <GuessedMarker />
+          ) : (
+            markersData.map((markers, index) => (
+              <MarkersContainer markers={markers} key={index} />
+            ))
+          )}
+        </MapContainer>
+      ) : null}
     </div>
   );
 };
