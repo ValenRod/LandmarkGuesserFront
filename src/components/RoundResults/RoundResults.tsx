58749401b16a9a0e3b83cd16ea9e61244ec63e96
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-04-09 16:23:41 +0200

    RoundResults component finished.

--- src/components/RoundResults/RoundResults.tsx
+++ src/components/RoundResults/RoundResults.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import './RoundResults.css';
+import {distanceInKm} from '../../utils/distanceInKm';
+import {useGameContext} from '../../hooks/useGameContext';
 
 export const RoundResults = () => {
-  return <div className="round-results"></div>;
+  const {gameParams} = useGameContext();
+  const {rounds} = gameParams;
+  const {distance} = rounds[rounds.length - 1];
+
+  return (
+    <div className="round-results">
+      <p className={'distance'}>
+        Your guess was
+        <span>{distance ? distanceInKm(distance) : null} km</span>
+        from the correct location.
+      </p>
+    </div>
+  );
 };
